c32438c86766bf39935a369240aeedb273ca9ce8
sidb236 <sid.unpredictable@gmail.com>
2023-05-17 13:20:34 +0530

    cosmetic changes in img_points.py

--- img_points.py.py
+++ img_points.py.py
@@ -2,29 +2,29 @@
 import cv2
 import os
 
-# of the points clicked on the image
-def click_event(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(f'({x},{y})')
+    # of the points clicked on the image
+    def click_event(event, x, y, flags, params):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print(f'({x},{y})')
 
-        # put coordinates as text on the image
-        cv2.putText(img, f'({x},{y})', (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            # put coordinates as text on the image
+            cv2.putText(img, f'({x},{y})', (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-        # draw point on the image
-        cv2.circle(img, (x, y), 3, (0, 255, 255), -1)
+            # draw point on the image
+            cv2.circle(img, (x, y), 3, (0, 255, 255), -1)
 
-        try:
-            # save the points in a txt file
-            path_file= './coordinates.txt'
-            if os.path.isfile(path_file) is True:
-                with open('coordinates.txt', 'a') as f:
-                    f.write(f'({x},{y})')
-                    f.write('\n')
-                f.close()
+            try:
+                # save the points in a txt file
+                path_file= './coordinates.txt'
+                if os.path.isfile(path_file) is True:
+                    with open('coordinates.txt', 'a') as f:
+                        f.write(f'({x},{y})')
+                        f.write('\n')
+                    f.close()
 
-        except Exception as e:
-            print(e)
+            except Exception as e:
+                print(e)
 
 
 
