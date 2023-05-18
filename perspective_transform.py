f7acccc036957915131bb75baaf790b6ca3b577e
sidb236 <sid.unpredictable@gmail.com>
2023-05-18 14:10:19 +0530

    some changes

--- perspective_transform.py
+++ perspective_transform.py
@@ -14,18 +14,18 @@ except Exception as e:
 print(img.shape)
 
 # define the source and destination points for perspective transform
-#src_points = np.float32([(0, 800), (1500, 800), (1000, 500), (500, 500)])
-#dst_points = np.float32([(0, img.shape[0]), (img.shape[1], img.shape[0]), (img.shape[1], 0), (0, 0)])
+src_points = np.float32([(0, 800), (1500, 800), (1000, 500), (500, 500)])
+dst_points = np.float32([(0, img.shape[0]), (img.shape[1], img.shape[0]), (img.shape[1], 0), (0, 0)])
 
 # compute the perspective transform matrix
-#M = cv2.getPerspectiveTransform(src_points, dst_points)
+M = cv2.getPerspectiveTransform(src_points, dst_points)
 
 # apply the perspective transform to the image
-#warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
+warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
 
 # display the original and transformed images side by side
-#cv2.imshow('Original Image', img)
-#cv2.imshow('Warped Image', warped)
+cv2.imshow('Original Image', img)
+cv2.imshow('Warped Image', warped)
 
 cv2.waitKey(0)
 
